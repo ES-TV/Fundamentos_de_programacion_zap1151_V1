4ff51369742c5437bfc68c25e16f5a8205c63277
STV <estebanchvzal@gmail.com>
2024-09-27 07:57:31 -0600

    C001_STV
    Crear repositorio y crear P001

---
+++ P001_HOLA_MUNDO/P001_HOLA_MUNDO.cpp
@@ -0,0 +1,50 @@
+// P001_HOLA_MUNDO.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <math.h>
+#include <time.h>
+#include <locale.h>
+#include <codecvt>
+#include <locale.h>
+
+int main()
+{
+    //maneras de configular para que soporte ñ y acentos
+    setlocale(LC_ALL, "es:MX.UTF-8");
+    //\n Es un salto de pagina
+    //st::endl
+    std::cout << "Hello World!\n";
+    //Inciaremos tipos de variables de diferentes tipos
+    //tipo nombre = valor asignado de declaracion o instalaciom
+    // TIPO GUARDAR ENTEROS REALES
+    int Edad = 0;
+    //para guardar buneros  decimales
+    float altura = 0;
+    float Cali = 0;
+    //para guardad verdades y falsedades
+    float d1 = false;
+    //para guardar numeros realmente extensos
+    double epobencial = 0;
+    //Papa raguardar letras
+    char sexo = " ";
+    //Para guardae cadenas de caracteres
+    char nombre[15];
+
+    //Hablemos con el usuario
+
+//
+//
+
+
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
