0f2847ba1135e4934292e5d720339b5a3fda09b1
ES-TV <estebanchvzal@gmail.com>
2024-10-07 08:54:01 -0600

    C002_ESTV
    CREAR ARCHIVO DE LA P007

---
+++ P007_Decisiones/P007_Decisiones/P007_Decisiones.cpp
@@ -0,0 +1,15 @@
+// P007_Decisiones.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <math.h>
+#include<locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "en_MX.UTF-8");
+    std::cout << "Weeeeell come to the Royal RRRRRRRuuuumble\n";
+    std::cout << "En este torneo se peleara a murte, elija el numero de jugadores (para que la pelea sea justa deben ser par los jugadores) \n";
+    std::cout << "(para que la pelea sea justa deben ser par los jugadores)\n";
+}
+
