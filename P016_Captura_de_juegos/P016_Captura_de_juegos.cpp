ec52f8dcc0a79e2426a2dc7a24b20f62c5ff06a8
4 <estebanchvzal@gmail.com>
2024-11-11 08:58:47 -0600

    C001_ESTV
    empezar practica 16 y C008

---
+++ P016_Captura_de_juegos/P016_Captura_de_juegos.cpp
@@ -0,0 +1,29 @@
+// P016_Captura_de_juegos.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//Esteban Chavez Alvarez
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <locale>
+#include <locale.h>
+#include <locale>
+#include <Windows.h>
+
+
+int main()
+{
+    setlocale(LC_ALL, "en_MX.UTF-8");
+    std::string titulo[3];
+    int anio[3];
+    std::string autor[3];
+    std::string company[3];
+    std::cout << "Vamos a registrar nuestros juegos favoritos, cuantos deseas poner?\n";
+    std::cin >> tamanio;
+    for (int i = 0; i < tamanio; i++)
+    {
+
+    }
+    
+}
+
+
