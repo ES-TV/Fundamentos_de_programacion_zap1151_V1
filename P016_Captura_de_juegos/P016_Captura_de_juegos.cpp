db25dfe1bd8335e4009614557d3a5fd84a0745ea
EstebanChvz <estebanchvzal@gmail.com>
2024-11-12 16:02:36 -0600

    C001_estv
    Avance P016

--- P016_Captura_de_juegos/P016_Captura_de_juegos.cpp
+++ P016_Captura_de_juegos/P016_Captura_de_juegos.cpp
@@ -17,11 +17,24 @@ int main()
     int anio[3];
     std::string autor[3];
     std::string company[3];
-    std::cout << "Vamos a registrar nuestros juegos favoritos, cuantos deseas poner?\n";
-    std::cin >> tamanio;
-    for (int i = 0; i < tamanio; i++)
+    std::cout << "Vamos a registrar nuestros juegos favoritos que genial\n";
+    for (int i = 0; i <=3; i++)
     {
-
+        std::cout << "Nombre del juego #" << i << std::endl;
+        std::getline(std::cin, titulo[i]);
+        std::cout << "Año de publicacion del juego #" << i << std::endl;
+        std::cin >> anio[i];
+        std::cout << "Autor del juego #" << i << std::endl;
+        std::getline(std::cin, autor[i]);
+        std::cout << "Estudio del juego #" << i << std::endl;
+        std::getline(std::cin, company[i]);
+    }
+    system("pause");
+    for (int i = 0; i <= 3; i++)
+    {
+        std::cout << "Juego #" << i;
+        std::cout << titulo[1];
+        std::cout << "año " << anio[i];
     }
     
 }
