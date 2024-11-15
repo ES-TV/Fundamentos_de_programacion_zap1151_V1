fabf1ee061ff62cae3701878c99e2e2d1f319107
s <estebanchvzal@gmail.com>
2024-11-15 09:01:12 -0600

    C002_estv
    Realizar P017

--- P017_Matrices/P017_Matrices.cpp
+++ P017_Matrices/P017_Matrices.cpp
@@ -17,29 +17,70 @@ int main()
     int matriz5[5][5];
     int matriz10[10][10];
     int op = 0;
-    std::cout << "Vamos a ver matrizes.\nEliga una opcion\n1) 3x3\n2) 5x5\n3) 10x10\n";
-    std::cin >> op;
-
+    bool seguir = 1;
     do
     {
+        std::cout << "Vamos a ver matrizes.\nEliga una opcion\n1) 3x3\n2) 5x5\n3) 10x10\n";
+        std::cin >> op;
         switch (op)
         {
         case 1:
-
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    matriz3[i][j] = rand() % 9 +1;
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    std::cout << "|" << matriz3[i][j] << "|";
+                }
+                std::cout << "\n";
+            }
+            seguir = 0;
             break;
         case 2: 
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    matriz5[i][j] = rand() % 9 + 1;
+                }
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    std::cout << "|" << matriz5[i][j] << "|";
+                }
+                std::cout << "\n";
+            }
+            seguir = 0;
             break;
         case 3:
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    matriz10[i][j] = rand() % 9 + 1;
+                }
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    std::cout << "|" << matriz10[i][j] << "|";
+                }
+                std::cout << "\n";
+            }
+            seguir = 0;
             break;
         default:
             std::cout << "Elige bien";
             break;
         }
-    } while (true);
-    switch (op)
-    {
-    default:
-        std::cout << "Elige bien";
-        break;
-    }
+    } while (seguir);
 }
