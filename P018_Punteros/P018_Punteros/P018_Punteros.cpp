114e8c25674581c6a0c5993f619db958194dfb7a
EstebanChvz <estebanchvzal@gmail.com>
2024-11-21 12:17:19 -0600

    C001_ES-TV
    Empezar P018

---
+++ P018_Punteros/P018_Punteros/P018_Punteros.cpp
@@ -0,0 +1,24 @@
+// P018_Punteros.cpp
+//Esteban Chavez Alvarez
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <locale>
+#include <locale.h>
+#include <locale>
+#include <Windows.h>
+#include <time.h> 
+
+int main()
+{
+    setlocale(LC_ALL, "es_MX.UTF-8");
+    int i = 0;
+    int j = 0;
+    int* matriz1 = NULL;
+    std::cout << "Vamos a crear una Matriz. \n¿De que tamaño desea?\n(Escriba primero la dimension i y despues la j)\n";
+    std::cin >> i;
+    std::cin >> j;
+
+}
+
