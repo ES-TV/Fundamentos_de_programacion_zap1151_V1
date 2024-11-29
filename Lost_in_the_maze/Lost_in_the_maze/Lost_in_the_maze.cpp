c3a941508646297c1ae18050159d91be8484f756
EstebanChvz <estebanchvzal@gmail.com>
2024-11-28 18:01:18 -0600

    C002_ES-TV
    Volver a empezar

---
+++ Lost_in_the_maze/Lost_in_the_maze/Lost_in_the_maze.cpp
@@ -0,0 +1,33 @@
+// Lost_in_the_maze.cpp 
+//Esteban Chávez Álvarez
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <locale>
+#include <locale.h>
+#include <locale>
+#include <Windows.h>
+#include <time.h> 
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "es_MX.UTF-8");
+    srand(time(NULL));
+    int camino;
+    int dado;
+    
+    std::string nombre;
+    std::cout << "Buenas...\n";
+    std::cout << "Escriba su nombre...\n";
+    std::getline(std::cin, nombre);
+    std::cout << "Te dpy la tragica bienvenida a lo que puede ser tu tumba...\n";
+    std::cout << "...\n";
+    std::cout << "(Te despiertas en medio de unos muros muy altos...)\n";
+    std::cout << "(Estas en un pequeño cuadrado y ves dos caminos...\n1"
+    system("pause");
+    
+}
+
--- Proyecto_Narracion/Proyecto_Narracion/Proyecto_Narracion.cpp
+++ Proyecto_Narracion/Proyecto_Narracion/Proyecto_Narracion.cpp
@@ -10,6 +10,7 @@
 #include <Windows.h>
 #include <time.h> 
 
+
 int main()
 {
 	setlocale(LC_ALL, "es_MX.UTF-8");
@@ -23,11 +24,23 @@ int main()
 	std::getline(std::cin, nombre);
 	std::cout << "Maravilloso " << nombre << ". Te doy la bienvenida a la base de espacial GalaxyCross\n";
 	std::cout << "la maquina de transporte mas grandre y rapida de la Via Lactea\n";
+	std::cout << "Mi nombre es Santiago, Santi, tiago, Chanti, Yago. Y soy el Teniente\n";
 	std::cout << "Estamos en una exploracion para estudiar de cerca el agujero negro supermasivo\n";
 	std::cout << "Sagitario A*\n";
 	std::cout << "Es de mi agrado comunicarle que debido a un infarto espacial el capitan ha muerto y ahora tu eres nuestro nuevo lider\n";
 	std::cout << "Asi que le actualizo\nEncontramos una estrella a docientos años luz con ningun planeta orbitando\nPor otro lado tenemos un pequeño sistema a cruzando dos estrellas màs\n";
-	std::cout << "Por lo que tenemos dos opciones\n1) Revisar a ver si uno de los planetas tiene recurson para la nave o intentar deborar la estrella paraq conseguir energia\n";
-	std::cout << "Que desea hacer capitan?\n"
+	std::cout << "Por lo que tenemos dos opciones\n1) Revisar a ver si uno de los planetas tiene recursos para la nave\n2) intentar deborar la estrella paraq conseguir energia\n";
+	std::cout << "Que desea hacer capitan?\n";
+	std::cin >> desicion1;
+	switch (desicion1)
+	{
+	case 1:
+		std::cout << "Muy bien capitan activemos velocidad interestelar!!!\n";
+		std::cout << "pssshhh vvvuuuuuuuu fiuuumm\n";
+		std::cout << "...\n";
+		std::cout << "Capitanazo!! Hemos llegado a un sistema "
+	default:
+		break;
+	}
 }
 
